a75c4e3bfbd74f69ba22ee2b9d834ed6d601f5ad
laibasami56 <laibasami9876@gmail.com>
2024-10-07 01:47:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//Question:01 Declare a variable called username.
+
+let username ;
+
+// Question : 02 Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+var myName = "Laiba Sami";
+
+// Question : 03 Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+
+// a) Declare a JS variable, titled message.
+var message ;
+
+ // b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in alert box.
+alert(message);
+
+//Question : 04 . Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+let name = "jhone doe";
+let age = "15 Years old " ;
+let detail = "Certified Mobile Application Development";
+
+alert(name);
+alert(age);
+alert(detail);
+
+//Question : 05 Write a script to display the following alert using one JS
+// variable:
+
+let text ;
+text = "PIZZA \nPIZZ \nPIZ \nPI \nP ";
+alert(text);
+
+//Question : 06 Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+let email ;
+email = "My email address is example @example.com" ;
+alert(email);
+
+
+//Question : 07  Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+ 
+let book = "I am trying to learn from the Book A smarter way to learn JavaScript " ;
+alert(book);
+
+
+// Question : 08 Write a script to display this in browser through JS
+
+let X = "yah! I can write HTML content through java script " ;
+document.write(X);
+
+//Question : 9 Store following string in a variable and show in alert and
+// browser through JS  “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+let y = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(y); 
